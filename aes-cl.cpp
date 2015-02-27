175dad91588ec9e3154a91222014e866099c4ba8
Kevin Yang <kyang3@caltech.edu>
2015-02-27 14:58:41 -0800

    Fixed Interactive AES Command-Line Tool
    I didn't realize previously that doing cin >> var
      tokenizes on all whitespace, so I was having issues
      with keys and text with spacing.  Replaced those
      statements with getline() calls.
    
    As it is now, this tool is probably fragile.  It assumes
      all ciphertext comes in unspaced hex characters with
      lowercase letters, and everything else is standard,
      normal people text.  Later I will try to fix it so
      it is more flexible, and add an ASCII to hex command-line
      conversion tool for flexibility.

--- aes-cl.cpp
+++ aes-cl.cpp
@@ -1,27 +1,70 @@
 #include "aes.hpp"
 #include "util.hpp"
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+string process_hex(string arg);
+
+string process_hex(string arg)
+{
+    string temp;
+    int val = 0;
+    if(arg.length() % 2 != 0)
+    {
+        throw invalid_argument("Argument hex string must be even length");
+    }
+    for(int i = 0; i < arg.length(); i += 2)
+    {
+        if(arg[i] < '0' || (arg[i] > '9' && arg[i] < 'a') || arg[i] > 'f')
+        {
+            throw invalid_argument("Unacceptable hex character");
+        }
+        if(arg[i+1] < '0' || (arg[i+1] > '9' && arg[i+1] < 'a') || arg[i+1] > 'f')
+        {
+            throw invalid_argument("Unacceptable hex character");
+        }
+        if(arg[i] >= '0' && arg[i] <= '9')
+        {
+            val += 16 * (arg[i] - '0');
+        }
+        else
+        {
+            val += 16 * ((int)arg[i] - 87);
+        }
+        if(arg[i+1] >= '0' && arg[i+1] <= '9')
+        {
+            val += arg[i+1] - '0';
+        }
+        else
+        {
+            val += (int)arg[i+1] - 87;
+        }
+        temp.push_back((char) val);
+        val = 0;
+    }
+    return temp;
+}
 
 int main()
 {
     cout << "AES-128 Interactive Encrypt/Decrypt Tool" << endl;
     cout << "Encrypt or decrypt?  Enter 0 for encrypt, 1 for decrypt." << endl;
-    int i;
-    cin >> i;
-    if(i != 1 && i != 0)
+    string i;
+    getline(cin, i);
+    if(i[0] != '1' && i[0] != '0')
     {
         cerr << "error: invalid option specified" << endl;
         return 1;
     }
-    else if(i == 0)
+    else if(i[0] == '0')
     {
         string ptext, key;
         cout << "Enter plaintext: " << endl;
-        cin >> ptext;
+        getline(cin, ptext);
         cout << "Enter key: " << endl;
-        cin >> key;
+        getline(cin, key);
         string ctext = aes_128_single_encrypt(ptext, key);
         cout << "The ciphertext in hex is: " << endl;
         hex_print(ctext);
@@ -30,12 +73,15 @@ int main()
     }
     else
     {
-        string ctext, key;
-        cout << "Enter ciphertext: " << endl;
-        cin >> hex >> ctext;
-        hex_print(ctext);
+        string ctext = "", key = "", temp;
+        cout << "Enter ciphertext in hex (lowercase chars please): " << endl;
+        getline(cin, ctext);
+        //hex_print(ctext);
+        ctext = process_hex(ctext);
+        //cout << "You got: \n";
+        //cout << ctext << endl;
         cout << "Enter key: " << endl;
-        cin >> key;
+        getline(cin, key);
         string ptext = aes_128_single_decrypt(ctext, key);
         cout << "The plaintext in hex is: " << endl;
         hex_print(ptext);
